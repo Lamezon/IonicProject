43df083fd338c27e4a43dafd8a62cff24d4ceebe
Matheus Lamezon <matheuslamezon@gmail.com>
2019-09-19 18:03:30 -0300

    Add elevation & accuracy

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -11,6 +11,8 @@ export class AboutPage {
   resp_coords: any;
   latitude: any;
   longitude: any;
+  elevation: any;
+  accuracy: any;
   constructor(public navCtrl: NavController, private geolocation: Geolocation) {
 
   }
@@ -21,15 +23,30 @@ export class AboutPage {
       this.resp_coords = resp.coords;
       this.latitude = this.resp_coords.latitude;
       this.longitude = this.resp_coords.longitude;
+      this.elevation = this.resp_coords.elevation;
+      this.accuracy = this.resp_coords.accuracy;
 
-  
      
+  
+
       console.log(resp);
     }).catch((error) => {
       console.log('Erro ao recuperar sua posição')
       console.log(error);
     });
+    let watch = this.geolocation.watchPosition();
+    watch
+      .subscribe((resp) => {
+        this.resp_coords = resp.coords;
+        this.latitude = this.resp_coords.latitude;
+        this.longitude = this.resp_coords.longitude;
+        this.elevation = this.resp_coords.elevation;
+        this.accuracy = this.resp_coords.accuracy;
 
+      },(error) => {
+        console.log(error);
+      });
+    
   }
   
 
